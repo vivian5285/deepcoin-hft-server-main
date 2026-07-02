7040e061af4d1fcf77d521a29b9dd858023a2f38
vivian5285 <wangjiali240@gmail.com>
2026-07-02 22:22:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def webhook():
 
 @app.route('/health', methods=['GET'])
 def health():
-    return jsonify({"status": "ok", "service": "deepcoin_webhook", "version": "v13.4.8-tp-radar-dingtalk"}), 200
+    return jsonify({"status": "ok", "service": "deepcoin_webhook", "version": "v13.4.10-recover-dingtalk"}), 200
 
 if __name__ == '__main__':
     host_ip = os.getenv("FLASK_HOST", "0.0.0.0")
